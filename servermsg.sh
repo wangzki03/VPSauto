a0ab9069d951bded7e81e63ce847a1f1a6690cf7
wangzki03 <35860608+wangzki03@users.noreply.github.com>
2018-03-31 12:58:12 +0800

    Create servermsg.sh

---
+++ servermsg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Script create banner
+
+clear
+echo -e ""
+echo -e "READ ME FIRST"
+echo -e ""
+echo -e "* To Save Changes in Nano Editor *"
+echo -e "Press Ctrl+X,"
+echo -e "Press Y,"
+echo -e "Press Enter,"
+echo -e ""
+echo -e ""
+echo -e "* To Close Nano Editor *"
+echo -e "Press Ctrl+Z"
+echo -e ""
+echo -e ""
+echo -e "* To Change Server Message *"
+echo -e "Please input the following command:"
+echo -e "nano /etc/issue.net"
+echo -e ""
+echo -e ""
