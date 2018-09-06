02710e7d862266b24767fd9789ecd1b496a03246
wangzki03 <35860608+wangzki03@users.noreply.github.com>
2018-09-06 14:52:29 +0800

    Add files via upload

---
+++ v1site.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script by : _Dreyannz_
+apt-get update
+apt-get install apache2 -y
+apt-get install libssh2-php -y
+service apache2 restart
+apt-get install nginx -y
+apt-get install php5-fpm -y
+apt-get install php5-cli -y
+apt-get -y install nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup 
+mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup 
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Dreyannz/AutoScriptVPS/master/Files/Nginx/nginx.conf" 
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Dreyannz/AutoScriptVPS/master/Files/Nginx/vps.conf" 
+sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini 
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+service nginx restart
+cd /home/vps/public_html
+wget https://raw.githubusercontent.com/Dreyannz/VPS_Site/master/Version%201.0/index.php
+
+
+
+
